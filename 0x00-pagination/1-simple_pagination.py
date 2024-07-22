fcbf6f0984f31d40001ca25c739e11a9856af375
willykar <103494595+willykar@users.noreply.github.com>
2024-07-22 22:57:27 +0300

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -20,6 +20,7 @@ def index_range(page: int, page_size: int) -> tuple:
 
 class Server:
     """Server class to paginate a database of popular baby names."""
+    
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
