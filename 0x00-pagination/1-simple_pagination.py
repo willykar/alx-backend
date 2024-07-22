9ebc0d708aacff7ebddbe2ca6301c0718ff4f2e0
willykar <103494595+willykar@users.noreply.github.com>
2024-07-22 22:40:20 +0300

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -8,10 +8,12 @@ from typing import List
 
 
 def index_range(page: int, page_size: int) -> tuple:
+    """ index_range that takes two integer arguments
+    page and page_size
+    Return: a tuple of size two containing a start
+    index and an end index
     """
-    Returns a tuple of size two containing a start index and an end index
-    corresponding to the range of indexes to return in a list for those particular pagination parameters.
-    """
+
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
@@ -50,4 +52,3 @@ class Server:
 
         # Return the appropriate page of the dataset
         return dataset[start_index:end_index]
-
