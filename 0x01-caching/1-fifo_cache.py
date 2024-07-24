e56a50b8879aa0b593428dd160523b771e72e091
willykar <103494595+willykar@users.noreply.github.com>
2024-07-24 19:09:47 +0300

    Create 1-fifo_cache.py

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" 1-fifo_cache module """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ Define FIFOCache class
+    that inherits from BaseCaching
+    """
+
+    def __init__(self):
+        """ Initialize FIFOCache """
+        self.queue = []
+        super().__init__()
+
+    def put(self, key, item):
+        """ A put method thats assigns
+        the item to the dictionary
+        """
+        if key and item:
+            if self.cache_data.get(key):
+                self.queue.remove(key)
+            self.queue.append(key)
+            self.cache_data[key] = item
+            if len(self.queue) > self.MAX_ITEMS:
+                delete = self.queue.pop(0)
+                self.cache_data.pop(delete)
+                print('DISCARD: {}'.format(delete))
+
+    def get(self, key):
+        """ A get method that outputs the
+        value associated with the given key
+        """
+        return self.cache_data.get(key)
